a67cf248af25a27beee00c6067cbff0241fb04e1
Burak <burak.kocer@me.com>
2017-11-27 23:42:12 +0300

    Parameter binding
    We can pass the parameter in the URL: /movies/edit/1  or in the query string: /movies/edit?id=1

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,10 @@ namespace Vidly.Controllers
             var movie = new Movie() {Name = "Shrek!"};
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
     }
 }
